939400cdbf1cc582198c4586399e57f897760a58
ankit <a2.ankitrai@gmail.com>
2016-07-05 22:39:12 +0530

    ll

--- DS/LL/LinkedList.java
+++ DS/LL/LinkedList.java
@@ -18,6 +18,42 @@ class LinkedList {
 			}
 		}
 
+		public void push(int new_data){
+			Node newNode = new Node(new_data);
+
+			newNode.next = this.head;
+			this.head = newNode;
+			//Time complexity of push() is O(1) as it does constant amount of work.
+		}
+
+		public void insertAfter(Node targetNode, int new_data){
+			if(targetNode==null){
+				System.out.println("Null node");
+			}
+			Node newNode = new Node(new_data);
+			newNode.next = targetNode.next;
+			targetNode.next = newNode;
+			//Time complexity of insertAfter() is O(1) as it does constant amount of work.
+		}
+
+		public void append(int new_data){
+
+			if(this.head==null){
+				this.head = new Node(new_data);	
+				return ;
+			}
+			Node n = this.head;
+			Node newNode = new Node(new_data);
+			while(n.next != null){
+				n=n.next;
+			}
+			n.next = newNode;
+
+			/***Time complexity of append is O(n) where n is the number of nodes in linked list. Since there is a loop from head to end, the function does O(n) work.
+				This method can also be optimized to work in O(1) by keeping an extra pointer to tail of linked list
+			**/
+		}
+
 		public static void main(String[] args) {
 			LinkedList ll = new LinkedList();
 
@@ -28,6 +64,11 @@ class LinkedList {
 			ll.head.next = n2;
 			n2.next = n3;
 
+			ll.push(24);
+			ll.insertAfter(n2,69);
+			ll.append(38);
+			ll.append(39);
+			ll.append(40);
 			ll.printList();				
 		}
 }
