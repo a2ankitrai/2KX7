62267fa3f7e9966575aed7b1170f9add928ec6b3
a2ankitrai <a2.ankitrai@gmail.com>
2016-08-03 11:46:33 +0530

    updated files

--- gitcommit.bat
+++ gitcommit.bat
@@ -2,18 +2,18 @@
 
 echo Staging newly added or modified files... 
 git add .
-echo
+echo:
 
 echo Commiting to local repository....
 git commit -m "updated files"
-echo
+echo:
 
 echo Configuring git username and user email...
 git config user.name "a2ankitrai"
 git config user.email a2.ankitrai@gmail.com
-echo
+echo:
 
 echo pushing to remote repository...
 git push origin master
-echo
+echo:
 
