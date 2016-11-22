7222c7474f161b2a06937bca90058313a78a191a
a2ankitrai <a2.ankitrai@gmail.com>
2016-11-22 16:58:41 +0530

    trie

---
+++ DS/Trie/TrieNode.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class TrieNode {
+
+	char c;
+	Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+	boolean isLeaf;
+
+	public TrieNode() {}
+	public TrieNode(char c) {
+		this.c = c;
+	}
+}
