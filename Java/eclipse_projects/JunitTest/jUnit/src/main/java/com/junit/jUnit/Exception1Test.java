6ab24d21869176b50108189039654b2b355efd09
a2ankitrai <a2.ankitrai@gmail.com>
2017-01-03 18:10:38 +0530

    junit

---
+++ Java/eclipse_projects/JunitTest/jUnit/src/main/java/com/junit/jUnit/Exception1Test.java
@@ -0,0 +1,18 @@
+package com.junit.jUnit;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class Exception1Test {
+
+	 @Test(expected = ArithmeticException.class)
+	    public void testDivisionWithException() {
+	        int i = 1 / 0;
+	    }
+
+	    @Test(expected = IndexOutOfBoundsException.class)
+	    public void testEmptyList() {
+	        new ArrayList<>().get(0);
+	    }
+}
