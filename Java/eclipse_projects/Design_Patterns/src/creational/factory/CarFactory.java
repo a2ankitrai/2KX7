a10cf4e0834614816f4d98274a7395396b7db942
a2ankitrai <a2.ankitrai@gmail.com>
2017-01-08 21:45:26 +0530

    des pat

---
+++ Java/eclipse_projects/Design_Patterns/src/creational/factory/CarFactory.java
@@ -0,0 +1,23 @@
+package creational.factory;
+
+public class CarFactory {
+	public static Car buildCar(CarType model){
+		Car car = null;
+		
+		switch (model) {
+		case SMALL:
+			car = new SmallCar();
+			break;
+		case SEDAN:
+			car = new SedanCar();
+			break;
+		case LUXURY:
+			car = new LuxuryCar();
+			break;	
+		default:
+			throw new IllegalArgumentException("Invalid car type");
+		}
+		
+		return car;
+	}
+}
