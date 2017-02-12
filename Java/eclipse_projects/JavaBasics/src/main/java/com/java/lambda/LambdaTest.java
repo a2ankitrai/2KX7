57cdfa8ee481a7e136db97549637bdfe9b1e65e1
a2ankitrai <a2.ankitrai@gmail.com>
2017-02-12 11:49:23 +0530

    clearing

---
+++ Java/eclipse_projects/JavaBasics/src/main/java/com/java/lambda/LambdaTest.java
@@ -0,0 +1,11 @@
+package com.java.lambda;
+
+public class LambdaTest {
+
+	public static void main(String[] args) {
+		
+		MyLambda m = () -> System.out.println("hello words");
+		
+		m.sample();
+	}
+}
---
+++ Java/eclipse_projects/JavaBasics/src/main/java/com/java/lambda/MyLambda.java
@@ -0,0 +1,6 @@
+package com.java.lambda;
+
+public interface MyLambda {
+
+	void sample();
+}
