b83b13906af81800770cdffabd7d391abd86710f
a2ankitrai <a2.ankitrai@gmail.com>
2017-03-13 09:00:39 +0530

    Same class runnable

--- Java/eclipse_projects/Spring/SpringTest/src/main/java/com/spring/rest/GreetingController.java
+++ Java/eclipse_projects/Spring/SpringTest/src/main/java/com/spring/rest/GreetingController.java
@@ -16,4 +16,6 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
     }
+    
+   
 }
