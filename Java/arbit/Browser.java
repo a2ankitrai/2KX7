0fb6ce8176771f1b64214688faf864bbbbad3233
a2ankitrai <a2.ankitrai@gmail.com>
2017-03-26 00:20:06 +0530

    -m

---
+++ Java/arbit/Browser.java
@@ -0,0 +1,28 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Browser {
+    public static void main(String[] args) {
+        String url = "http://www.ankitrai.com";
+
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("xdg-open " + url);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
